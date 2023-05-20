b4d4cddf53c39ef2a03b35197265d073d9227577
ISMAILTODAY <ismail878708@gmail.com>
2023-05-20 11:25:20 +0600

    aking alldata from mongodb done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS)
-console.log(process.env.DB_USER)
+// console.log(process.env.DB_PASS)
+// console.log(process.env.DB_USER)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cbzwi67.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,6 +29,87 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
+
+        const toyDb = client.db('allToysData');
+        const toyCollection = toyDb.collection('toyData')
+
+
+        app.get("/alldata", async (req, res) => {
+            console.log(req.query.sort)
+            const sorts = { price: req.query.sort }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+
+            const result = await toyCollection.find(query).sort(sorts).toArray();
+            res.send(result);
+            console.log(result)
+        })
+
+
+
+        app.get('/alldata', async (req, res) => {
+            // console.log(req.query)
+            const cursor = toyCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.post('/alldata', async (req, res) => {
+            const addToys = req.body;
+            // console.log(req.body)
+            const result = await toyCollection.insertOne(addToys);
+            res.send(result);
+        })
+
+        app.put('/alldata/:id', async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            console.log(user)
+            const filter = { _id: new ObjectId(id) }
+            const option = { upsert: true }
+            const updatedUser = {
+                $set: {
+                    quantity: user.quantity,
+                    price: user.price,
+                    description: user.description
+
+                }
+            }
+            const result = await toyCollection.updateOne(filter, updatedUser, option);
+            res.send(result);
+        })
+
+        app.delete('/alldata/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await toyCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // app.get("/dataascending", async (req, res) => {
+        //     console.log(req.query.email)
+        //     let query = {};
+        //     if (req.query?.email) {
+        //         query = { email: req.query.email }
+        //     }
+
+        //     const result = await toyCollection.find(query).sort({ price: 1 }).toArray();
+        //     res.send(result);
+        //     console.log(result)
+        // })
+
+
+
+
+
+
+
+
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
